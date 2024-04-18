065fd8963208db322ac0b1312276186e84509f6c
Osaid Khan <2021CS145@student.uet.edu.pk>
2024-04-18 22:33:16 +0500

    Complete 'Add task' feature

---
+++ app.js
@@ -0,0 +1,22 @@
+let textBox = document.querySelector("#input-box");
+let btn = document.querySelector("button");
+let ul = document.querySelector("#list-container");
+let crossIcon = document.querySelector("span");
+
+
+btn.addEventListener('click', () => {
+    if (textBox.value == '') {
+        alert('You must write something!');
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerText = textBox.value;
+        let span = document.createElement('span');
+        span.innerText = '\u00d7';
+        li.appendChild(span);
+        ul.appendChild(li);
+    }
+    textBox.value = '';
+
+
+});
