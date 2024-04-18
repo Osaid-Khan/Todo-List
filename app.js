705140cd3e602a99cf8c9ff59aed8c7b3fe18b3d
Osaid Khan <2021CS145@student.uet.edu.pk>
2024-04-18 23:58:09 +0500

    Add 'Local Storage' feature

--- app.js
+++ app.js
@@ -15,16 +15,57 @@ btn.addEventListener('click', () => {
         span.innerText = '\u00d7';
         li.appendChild(span);
         ul.appendChild(li);
+        // Save task to Local Storage whenever 'li' added
+        saveTasksToLocalStorage();
     }
     textBox.value = '';
 
 });
 
 ul.addEventListener('click', (e) => {
-    if (e.target.nodeName == 'SPAN'){
+    if (e.target.nodeName == 'SPAN') {
         e.target.parentElement.remove();
+        // Save task to Local Storage whenever 'li' removed
+        saveTasksToLocalStorage();
     }
-    else if (e.target.nodeName == "LI"){
+    else if (e.target.nodeName == "LI") {
         e.target.classList.toggle("checked");
+        // Save task to Local Storage whenever 'li' status changed
+        saveTasksToLocalStorage();
     }
 });
+
+
+// Function to save tasks to local storage
+function saveTasksToLocalStorage() {
+    let tasks = [];
+    ul.querySelectorAll('li').forEach(item => {
+        tasks.push({
+            text: item.innerText.trim().replace(/\u00d7$/, '').replace(/\n/g, ''),
+            checked: item.classList.contains('checked')
+        });
+    });
+   
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to load tasks from local storage
+function loadTasksFromLocalStorage() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+        tasks.forEach(task => {
+            let li = document.createElement("li");
+            li.innerText = task.text;
+            if (task.checked) {
+                li.classList.add('checked');
+            }
+            let span = document.createElement('span');
+            span.innerText = '\u00d7';
+            li.appendChild(span);
+            ul.appendChild(li);
+        });
+    }
+}
+
+// Load tasks from local storage when the page is loaded
+window.addEventListener('DOMContentLoaded', loadTasksFromLocalStorage);
